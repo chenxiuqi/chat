23de952a64d72393276b5a8389783d991ff91cf3
chenxiuqi <xiuqichen8@gmail.com>
2017-06-26 15:40:33 -0700

    Updated AppDelegate to include Parse

--- chat/AppDelegate.swift
+++ chat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration) in
+            configuration.applicationId = "parseChatID"
+            configuration.server = "https://blooming-thicket-89345.herokuapp.com/parse"
+        }))
         return true
     }
 
@@ -40,7 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+   
 }
 
